1550ee9a31166d34c04957f73678cf5e4ecec294
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2019-10-17 11:35:28 +0200

    Add files via upload

---
+++ kostroEugenKreiranjeProjekta2Zadatak/kostroEugenKreiranjeProjekta2Zadatak/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenKreiranjeProjekta2Zadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float y = 18.5F;
+            double z = Convert.ToInt32(y);
+            long x = Convert.ToInt32(y);
+
+            Console.WriteLine("y = 18.5");
+            Console.WriteLine("z = {0}", y);
+            Console.WriteLine("x = {0}", y);
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
